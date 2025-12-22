5bbd23d2b382cf926bcdddc9d571b3b14fd7fa3c
Ucatatsu <hajemuru@hotmail.com>
2025-12-22 20:54:56 +0700

    Исправлено дублирование select - полностью скрыт оригинальный элемент

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // === КВАНТ - SERVICE WORKER ===
 
-const CACHE_NAME = 'kvant-v59';
+const CACHE_NAME = 'kvant-v60';
 const APP_VERSION = '1B0d18'; // KVS версия - синхронизируй с package.json
 const STATIC_ASSETS = [
     '/',
